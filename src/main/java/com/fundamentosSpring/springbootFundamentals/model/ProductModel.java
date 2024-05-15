823448bcfff12a2b0bd3e94f84978e753b57c015
Lima-Developer <jvflima14@gmail.com>
2024-05-14 23:37:44 -0300

    feat: create Product Entity

---
+++ src/main/java/com/fundamentosSpring/springbootFundamentals/model/ProductModel.java
@@ -0,0 +1,25 @@
+package com.fundamentosSpring.springbootFundamentals.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+//Informa que essa classe é uma entidade no banco de dados
+@Entity
+@Table(name = "TB_PRODUCTS")
+@Getter
+@Setter
+public class ProductModel implements Serializable  {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct; //A tipagem 'UUID' é uma tipagem de identificador
+    private String name;
+    private BigDecimal value;
+}
