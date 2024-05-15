ea78c960c6b1948c1753c43186aa7826f54a903f
Lima-Developer <jvflima14@gmail.com>
2024-05-14 23:37:28 -0300

    feat: initial config

---
+++ src/main/java/com/fundamentosSpring/springbootFundamentals/SpringbootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosSpring.springbootFundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosSpring/springbootFundamentals/SpringbootFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosSpring.springbootFundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
