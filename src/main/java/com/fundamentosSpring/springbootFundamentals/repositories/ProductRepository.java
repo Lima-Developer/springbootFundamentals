85fe4b61d7c6d1584f8215efcd1011b8fcb094fe
Lima-Developer <jvflima14@gmail.com>
2024-05-14 23:37:53 -0300

    feat: create Product Repository Entity

---
+++ src/main/java/com/fundamentosSpring/springbootFundamentals/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.fundamentosSpring.springbootFundamentals.repositories;
+
+
+import com.fundamentosSpring.springbootFundamentals.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
